20eb7a693d9dc15b01371fa53968fd50a4d35166
jfonseca77 <154247538+jfonseca77@users.noreply.github.com>
2024-06-17 20:23:21 +0100

    Delete src directory

--- src/models/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './armazem.model';
-export * from './encomenda.model';
-export * from './loja.model';
-export * from './cliente.model';
-export * from './pagamento.model';
-export * from './peca.model';
